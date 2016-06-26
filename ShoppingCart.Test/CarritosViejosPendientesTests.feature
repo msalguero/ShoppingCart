c0530dd47fc5155519afb252a243ec0f612283a2
vicra <vicramirez_10@hotmail.com>
2016-06-25 23:01:00 -0600

    Adding test files for feature and steos

---
+++ ShoppingCart.Test/CarritosViejosPendientesTests.feature
@@ -0,0 +1,13 @@
+﻿Feature: CarritosViejosPendientesTests
+	Revisar los carritos viejos pendientes
+
+Scenario: Revisar carritos viejos pendientes
+Given una lista de carritos pendientes y pagados
+| Id | Usuario   | Estado   | FechaDeCreacion |
+| 1  | victor    | sinPagar | 25/06/2016      |
+| 2  | ricardo   | sinPagar | 25/06/2016      |
+| 3  | sebastian | pagado   | 25/06/2016      |
+| 3  | edwin     | pagado   | 25/06/2016      |
+Given el usuario victor
+When Al ejecutar el reporte de carritos pendientes
+Then devolvera los carritos que tengan mas de un mes de creados y que aun esten pendientes
---
+++ ShoppingCart.Test/CarritosViejosPendientesTestsSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ShoppingCart.Test
+{
+    [Binding]
+    public class CarritosViejosPendientesTestsSteps
+    {
+        [Given(@"una lista de carritos pendientes y pagados")]
+        public void GivenUnaListaDeCarritosPendientesYPagados(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"el usuario (.*)")]
+        public void GivenElUsuario(string usuario)
+        {
+            ScenarioContext.Current.Add("usuario", usuario);
+        }
+        
+        [When(@"Al ejecutar el reporte de carritos pendientes")]
+        public void WhenAlEjecutarElReporteDeCarritosPendientes()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"devolvera los carritos que tengan mas de un mes de creados y que aun esten pendientes")]
+        public void ThenDevolveraLosCarritosQueTenganMasDeUnMesDeCreadosYQueAunEstenPendientes()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
