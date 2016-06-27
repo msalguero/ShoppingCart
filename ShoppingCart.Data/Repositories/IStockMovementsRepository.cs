95989d41f9abc91d7209f85ae1c6a5ae093230d3
msalguero <msalguero@unitec.edu>
2016-06-27 00:30:23 -0600

    added fifth scenario

--- ShoppingCart.Data/Repositories/IStockMovementsRepository.cs
+++ ShoppingCart.Data/Repositories/IStockMovementsRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShoppingCart.Data.Entities;
 
 namespace ShoppingCart.Data.Repositories
 {
     public interface IStockMovementsRepository
     {
+        List<StockMovements> GetMovements();
     }
 }
--- ShoppingCart/InventoryManager.cs
+++ ShoppingCart/InventoryManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShoppingCart.Data.Entities;
 using ShoppingCart.Data.Repositories;
 
 namespace ShoppingCart
@@ -15,5 +16,39 @@ namespace ShoppingCart
         {
             _stockRepository = stockRepository;
         }
+
+        public List<Report> GetReport()
+        {
+            var list = new List<Report>();
+            var movements = _stockRepository.GetMovements();
+            foreach (var move in movements)
+            {
+                AddMove(move, list);
+            }
+            return list;
+        }
+
+        private void AddMove(StockMovements move, List<Report> list)
+        {
+            foreach (var report in list)
+            {
+                if (report.ProductId == move.ProductId)
+                {
+                    report.Quantity += move.Quantity;
+                    return;
+                }
+            }
+            var newReport = new Report();
+            newReport.ProductId = move.ProductId;
+            newReport.Quantity = move.Quantity;
+
+            list.Add(newReport);
+        }
+    }
+
+    public class Report
+    {
+        public int ProductId;
+        public int Quantity;
     }
 }
